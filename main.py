7f9bfbdc6eae19bb7bca5685942e080d99c8d1f1
renanMM <r.mariano.m@gmail.com>
2026-03-10 22:17:36 -0300

    feat: Add first visuals to the game

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+import pygame
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+player = pygame.Rect((300, 200, 50, 50))
+
+def GameLoop():
+	run = True
+	while run:
+		screen.fill((0,0,0))
+		pygame.draw.rect(screen, (255, 0, 0), player)
+		key = pygame.key.get_pressed()
+
+		if key[pygame.K_a] == True:
+			player.move_ip(-1, 0)
+		if key[pygame.K_d] == True:
+			player.move_ip(1, 0)
+		if key[pygame.K_w] == True:
+			player.move_ip(0, -1)
+		if key[pygame.K_s] == True:
+			player.move_ip(0, 1)
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		pygame.display.update()
+
+
 def main():
-    print("Hello from race-game!")
+	GameLoop()
+	pygame.quit()
+	print("Hello from race-game!")
 
 
 if __name__ == "__main__":
-    main()
+	main()
